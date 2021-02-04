fa0756290e96e9f249b91fb58baf6e664b539f81
Aleksey Mikhailov <Aleksey.Mikhailov@icerockdev.com>
2021-02-04 20:04:58 +0700

    kotlin 1.4.30

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,7 +3,7 @@
  */
 
 plugins {
-    val kotlinVersion = "1.4.21"
+    val kotlinVersion = "1.4.30"
 
     id("org.jetbrains.kotlin.jvm") version (kotlinVersion)
     id("org.jetbrains.kotlin.plugin.serialization") version (kotlinVersion)
